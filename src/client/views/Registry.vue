f0715bc472ee7f81affcebf1249e0e612c43e9b1
Unknown <unknown@example.com>
2019-08-25 01:14:33 -0500

    cambios en posicion de registro

--- src/client/views/Registry.vue
+++ src/client/views/Registry.vue
@@ -1,5 +1,8 @@
 <template>
 <div class="container-registry">
+    <div class="questions">
+      <img src="../assets/question.png" alt="question">
+  </div>
  <div class="registry">
     <form>
       <div class="form-group">
@@ -9,9 +12,6 @@
         <input type="email" class="form-registry" id="exampleFormControlInput1" placeholder="name@example.com">
       </div>
     </form>
-  </div>
-  <div class="questions">
-      <img src="../assets/question.png" alt="question">
   </div>
 </div>
 </template>
